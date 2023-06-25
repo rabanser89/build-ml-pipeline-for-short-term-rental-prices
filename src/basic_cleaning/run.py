ee430e227f5a8f6a182d027c68f954eddc42c7ac
s.rabanser <s.rabanser@p8.eu>
2023-06-25 12:39:19 +0200

    add comments

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -29,7 +29,7 @@ def go(args):
     #Change date format from string to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
 
-    
+    logger.info("Longitude and latitude boundaries for properties in and around NYC")
     idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
     df = df[idx].copy()
 
